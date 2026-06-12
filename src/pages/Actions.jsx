8919ba2d68bbd2a5b227bf0be87cf06757e7bc69
Hamed Ftaich <hamedftaich84@gmail.com>
2026-06-12 14:06:53 +0200

    Ajout date creation et echeance actions

--- src/pages/Actions.jsx
+++ src/pages/Actions.jsx
@@ -35,7 +35,10 @@ export default function Actions() {
       alert("Veuillez remplir tous les champs");
       return;
     }
-
+    if (!echeance) {
+      alert("Veuillez entrer la date d'échance");
+      return;
+    }
     const { error } = await supabase.from("actions_correctives").insert([
       {
         titre,
